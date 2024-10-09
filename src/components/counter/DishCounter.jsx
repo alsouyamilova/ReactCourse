fdc4d4ffee773af91360f90e2d0f3fd0e2824766
alsouyamilova <alsouyamilova@gmail.com>
2024-10-09 21:22:03 +0300

    redux

--- src/components/counter/DishCounter.jsx
+++ src/components/counter/DishCounter.jsx
@@ -7,27 +7,6 @@ import {
 } from "../../redux/uiCart";
 
 export const DishCounter = ({ id }) => {
-  console.log(id);
-  //const [value, setValue] = useState(0);
-  // const increase = () => {
-  //   let newVal;
-  //   if (value + 1 > 5) {
-  //     newVal = 5;
-  //   } else {
-  //     newVal = value + 1;
-  //   }
-  //   setValue(newVal);
-  // };
-  // const decrease = () => {
-  //   let newVal;
-  //   if (value - 1 < 0) {
-  //     newVal = 0;
-  //   } else {
-  //     newVal = value - 1;
-  //   }
-  //   setValue(newVal);
-  // };
-
   const amount = useSelector((state) => selectAmountById(state, id));
   const dispatch = useDispatch();
   const increase = () => dispatch(addToCart(id));
